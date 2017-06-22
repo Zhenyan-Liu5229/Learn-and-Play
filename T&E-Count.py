9e1635d6452f18b518d2545144282540d6c0727e
Zhenyan-Liu5229 <Zhenyan-Liu5229@users.noreply.github.com>
2017-06-22 10:53:07 +0800

    Update and rename T&E-Count to T&E-Count.py

--- T&E-Count.py
+++ T&E-Count.py
@@ -6,28 +6,28 @@ F = [10,11,12,12,13,13,14,14,15,15,16,16,17,17,19,19,20,20,21,22]
 L = ['E1','E2','E3','E4','F','D','L']
 while n > 0:
     Step = input()
-    if  Step == L[0]: #一级敌刀
+    if  Step == L[0]: #Enemy at First level
         Exp = Exp+960
         Token = Token+30
-    elif Step == L[1]: #二级敌刀
+    elif Step == L[1]: #Enemy at Second level
         Exp = Exp+1200
         Token = Token+33
-    elif Step == L[2]: #三级敌刀
+    elif Step == L[2]: #Enemy at Third level
         Token = Token+36
         Exp  = Exp+1440
-    elif Step ==L[3]: #四级敌刀
+    elif Step ==L[3]: #Enemy at Fourth level
         Token = Token+45
         Exp = Exp+1780
-    elif Step ==L[4]: #随机花札并抽出
+    elif Step ==L[4]: #Random select Token-Card
         a = random.choice(F)
         Token = Token+a
         F.remove(a)
-    elif Step ==L[5]: #双倍花扎
+    elif Step ==L[5]: #Double-Token Card selected
         def d(x):
             return x*2    
         DF = map(d,F)
         F = list(DF)
-    elif Step ==L[6]: #陷阱掉花
+    elif Step ==L[6]: #Lose Tokens
         b = random.randint(1,9)
         Token = Token-b
     print(Token,Exp)
