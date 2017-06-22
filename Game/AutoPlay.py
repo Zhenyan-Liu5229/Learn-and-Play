2455df4eb78cdc3a76d63685cf6e83db1fcf4e58
Zhenyan Liu <Zhenyan-Liu5229@users.noreply.github.com>
2017-06-22 18:19:09 +0800

    Update AutoPlay.py
    This code is an simulation of the Japanese web game 刀剣乱舞. 
    It simulates the player's adventure to pick up tokens (and fight with enemies if unfortunate).

--- Game/AutoPlay.py
+++ Game/AutoPlay.py
@@ -1,26 +1,26 @@
 import random   
 a=0
-n=15
+n=14 #Steps in total
 exp=0
 tok=0
-I = ['D','L']
+I = ['D','L'] #Factors that influence token number. 'D'represents 'double' and 'L'represents 'loss'.
 F = [10,11,12,12,13,13,14,14,15,15,16,16,17,17,19,19,20,20,21,22]
-P = [1,1,1,10,11,12,12,13,13,14,14,15,15,16,16,17,17,19,19,20,20,21,22,'D','D','L','L','LE','LE','H','H']
+P = [1,1,1,10,11,12,12,13,13,14,14,15,15,16,16,17,17,19,19,20,20,21,22,'D','D','L','L','LE','LE','H','H'] #The pool of cards. 'LE'represents 'lose equipment' and 'H' represents 'hit damage'. 
 while n > 0:
     S = random.choice(P)
     if S == 1:
-        P[P.index(1)]=2
+        P[P.index(1)]=2 #After the first battle, the enemy will level up. The highest level is the 4th.
         exp=exp+960
         tok=tok+30        
     elif S == 2:
-        P[P.index(2)]=3
+        P[P.index(2)]=3 #level 2
         exp=exp+1200
         tok=tok+33
     elif S == 3:
-        P[P.index(3)]=4
+        P[P.index(3)]=4 #level 3
         exp=exp+1440
         tok=tok+36       
-    elif S == 4:
+    elif S == 4: #level 4
         exp=exp+1780
         tok=tok+45
         P.remove(S)        
