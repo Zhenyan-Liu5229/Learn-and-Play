66b8e4828a3377c4b106cf5582caa1ba0ea3ac9c
Zhenyan Liu <Zhenyan-Liu5229@users.noreply.github.com>
2017-06-30 15:57:55 +0800

    Update IFofDL.py

--- Game/IFofDL.py
+++ Game/IFofDL.py
@@ -3,6 +3,25 @@
 #I have wrote a paper draft of it as the final project of that course.
 #The code I put here are based on the toturial from https://randomgeekery.org/post/2007/01-handling-a-single-round/.
 
+def print_header():
+    print('''
+    _            _      _             _                  _     _        
+   /_\  _ _     /_\  __| |_ _____ _ _| |_ _  _ _ _ ___  (_)_ _| |_ ___  
+  / _ \| ' \   / _ \/ _` \ V / -_) ' \  _| || | '_/ -_) | | ' \  _/ _ \ 
+ /_/_\_\_||_| /_/ \_\__,_|\_/\___|_||_\__|\_,_|_| \___| |_|_||_\__\___/ 
+ |   \(_)__ _(_) |_ __ _| | | |  (_) |_ ___ _ _ __ _| |_ _  _ _ _ ___   
+ | |) | / _` | |  _/ _` | | | |__| |  _/ -_) '_/ _` |  _| || | '_/ -_)  
+ |___/|_\__, |_|\__\__,_|_| |____|_|\__\___|_| \__,_|\__|\_,_|_| \___|  
+        |___/    
+        ''')
+print_header()
+print('''
+Welcome to An Adventure into Digital Literature!
+You are one of students enrolled in ENG342 paper.
+Now, your adventure start...''')
+
+delay = input('>>> Press ENTER to start <<<')
+
 from Scene import scenes
 import sys
 scene = scenes['Start']
@@ -36,6 +55,6 @@ while True:
         print('You decide to',next_step,'.','Good Bye!')
         sys.exit()
     else:
-        scene = scenes[next_step['look_at']]
+        scene = scenes[next_step['do']]
         a = str.lower(next_step['phrase'])
         print('You decide to',a,'.')
