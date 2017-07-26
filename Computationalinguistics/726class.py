49930d847f882e66a6b87dacb51e9dd0625eb6c2
Zhenyan Liu <Zhenyan-Liu5229@users.noreply.github.com>
2017-07-26 09:55:10 -0700

    Update 726class.py

--- Computationalinguistics/726class.py
+++ Computationalinguistics/726class.py
@@ -11,3 +11,22 @@ for base_symbol in grammar['B']:
     sentence.append(word)
 sentence = ' '.join(sentence)
 print(sentence)
+
+def generate(symbol, output):
+    if symbol.isupper():
+        for s in grammar[symbol]:
+            generate(s, output)
+    else:
+        output.append(symbol)
+    return output
+
+grammar = {'S':['A','B'],
+            'A':['C','a'],
+            'B':['A','b'],
+            'C':['c']
+            }
+
+output = []
+for symbol in grammar['S']:
+    generate(symbol,output)
+print(output)
