81b0c87127d4e2867be91bfd2b65d64d6d51f727
Zhenyan Liu <Zhenyan-Liu5229@users.noreply.github.com>
2017-07-27 14:50:19 -0700

    Update 727class.py

--- Computationalinguistics/727class.py
+++ Computationalinguistics/727class.py
@@ -4,27 +4,31 @@ symbol = ['C','V']
 state = ['q0','q1','q2']
 transitions = {'q0':['q1','q2'],
               'q1':['q1','q2'],
-              'q2':['q1']}
+              'q2':'q1'}
 
-def transition(letters, current_state = state[0]):
+def transition(letters, current_state):
+    current_state = 'q0'
     for letter in letters:
-        if letter == symbol[0]:
-            current_state = transitions[current_state][1]
-            if letter == symbol[1]:
-                current_state = transitions[current_state][0]
+        if current_state == state[0]:
+            if letter == symbol[0]:
+                current_state = transitions[current_state][1]
             else:
-                break
-        elif letter == symbol[1]:
-            current_state = transitions[current_state][0]
-            if letter ==symbol[0]:
-                transition(letter)
-                
-        
-    if current_state == state[1]:
-        print('Ture')
+                current_state = transitions[current_state][0]
+        elif current_state == state[1]:
+            if letter == symbol[0]:
+                current_state = transitions[current_state][1]
+            else:
+                current_state = transitions[current_state][0]
+        elif current_state == state[2]:
+            if letter == symbol[1]:
+                current_state = transitions[current_state]
+            else:
+                break   
+    if current_state == 'q1':
+        print('True')
     else:
-        print('False')
+        print('False')        
 
-while True:
+while True:   
     letters = input()
-    transition(letters)
+    transition(letters, 'q0')
