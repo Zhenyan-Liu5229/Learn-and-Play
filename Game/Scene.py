69bf7f2424e1c1ed039fc5bf92a7ae86949c8f5e
Zhenyan Liu <Zhenyan-Liu5229@users.noreply.github.com>
2017-08-03 11:11:02 -0700

    Update Scene.py

--- Game/Scene.py
+++ Game/Scene.py
@@ -8,8 +8,13 @@ scenes = {
     The computer in front of you requires your user name and password.
     ''',
     'paths':[
-    {'do':'enter','phrase':'type your username'}
+    {'do':'enter', 'phrase':'type your username'},
+    {'do':'shut_down', 'phrase':'shut down the computer'}
     ]
+},
+    'shut_down':{
+    'description':'You think that you probably remember the wrong class time.It is better to shut down the computer and leave.',
+    'paths':[]
 },
     'enter':{
     'description':'''As soon as you finish typing, the password appears automatically on the screen, and the computer unlocks. 
@@ -17,7 +22,7 @@ scenes = {
     One hand reaches from the computer screen and drags you into it.
     ''',
     'paths':[
-    {'do':'start','phrase':'look around'}
+    {'do':'start', 'phrase':'look around'}
     ]
 },
     'start':{
@@ -26,21 +31,24 @@ scenes = {
     There are four screens you can choose to look at.
     ''',
     'paths':[
-    {'do':'No.7','phrase':'Look at No.7'},
-    {'do':'No.2','phrase':'Look at No.2'},
-    {'do':'No.3','phrase':'Look at No.3'},
-    {'do':'No.10','phrase':'Look at No.10'}
+    {'do':'No.7', 'phrase':'Look at No.7'},
+    {'do':'No.2', 'phrase':'Look at No.2'},
+    {'do':'No.3', 'phrase':'Look at No.3'},
+    {'do':'No.10', 'phrase':'Look at No.10'}
     ]
 },
+
+    #The Stanley parable
     'No.7':{
-    'description':'''You are playing The Stanley Parable. 
+    'description':'''Workshop7 -- The Stanley Parable
+    You are playing The Stanley Parable. 
     Now your character, Stanley, is facing two open doors. 
     A loud male voice is repeating “Stanley chooses the door on his left”. 
     Are you going to obey him?
     ''',
     'paths':[
-    {'do':'obey','phrase':'Move Stanley to the left door'},
-    {'do':'disobey','phrase':'Move Stanley to the right door'}
+    {'do':'obey', 'phrase':'Move Stanley to the left door'},
+    {'do':'disobey', 'phrase':'Move Stanley to the right door'}
     ]
 },
     'obey':{
@@ -49,9 +57,9 @@ scenes = {
     You are in the world center again.
     ''',
     'paths':[
-    {'do':'No.3','phrase':'Look at No.3'},
-    {'do':'No.10','phrase':'Look at No.10'},
-    {'do':'No.2','phrase':'Look at No.2'}
+    {'do':'No.3', 'phrase':'Look at No.3'},
+    {'do':'No.10', 'phrase':'Look at No.10'},
+    {'do':'No.2', 'phrase':'Look at No.2'}
     ]
 },
     'disobey':{
@@ -62,22 +70,48 @@ scenes = {
     ''',
     'paths':[]
 },
+
+    #hypertext 253
     'No.3':{
-    'description':'''You are reading 253, a hypertext about a London underground train. 
+    'description':'''Workshop3 -- Hypertext
+    You are reading 253, a hypertext about a London underground train. 
     There is a map of passengers in car 2 of that train. 
     Each passenger has his/her interests written below his/her name.
     ''',
     'paths':[
-    {'do':'No.7','phrase':'Look at No.7'},
-    {'do':'No.10','phrase':'Look at No.10'},
-    {'do':'No.2','phrase':'Look at No.2'},
-    {'do':'No.6','phrase':'Look at No.6'}
+    {'do':'passenger63', 'phrase':'select passenger 63'}, 
+    {'do':'passenger41', 'phrase':'select passenger 41'}, 
+    {'do':'close_screen', 'phrase':'close the screen'}
+    ]
+},
+    'passenger63':{
+    'description':'''Passenger 63:Oliver Maskey. For more information, visit http://www.ryman-novel.com/car2/63.htm.\
+    I hope you will find it interesting!''',
+    'paths':[]
+},
+    'passenger42':{
+    'description':'''Passenger 41:Mr Chris Green. For more information, visit http://www.ryman-novel.com/car2/41.htm\
+    I hope you will find it interesting!
+    And I tell you --
+    that the train will crash in the end.''',
+    'paths':[]
+},
+    'close_screen':{
+    'description':'''You decide not to read this hypertext. You don't like to read and click.
+    Instead, you turn around, and look at other screens.''',
+    'paths':[
+    {'do':'No.7', 'phrase':'Look at No.7'},
+    {'do':'No.10', 'phrase':'Look at No.10'},
+    {'do':'No.2', 'phrase':'Look at No.2'},
+    {'do':'No.6', 'phrase':'Look at No.6'}
     ]
 },
     
+    #telescopic texts
     'No.2':{
-    'description':'''You are writing a telescopic text. 
-    For your sake, please do not lick here. 
+    'description':'''Workshop2 -- Telescopic Text
+    You are writing a telescopic text. 
+    For your sake, please do not visit http://www.telescopictext.org/text/kEVYF0kNK18qY. 
     When you finish reading, screen No.2 is replaced by No.5.
     ''',
     'paths':[
@@ -88,8 +122,10 @@ scenes = {
     ]
 },
     
+    #Shadow of Colussus
     'No.5':{
-    'description':'''You are playing Shadow of Colossus, an exciting PS4 game.
+    'description':'''Workshop5 -- Game and Narratives I 
+    You are playing Shadow of Colossus, an exciting PS4 game.
     This is the first time you play with a Joystick.
     Of course, you are not playing for fun.
     You need to write a 2500-word essay about it.
@@ -101,8 +137,10 @@ scenes = {
     ]
 },
     
+    #The God of War
     'No.6':{
-    'description':'''Another game? Yeah!
+    'description':'''Workshop6 -- Game and Narratives II 
+    Another game? Yeah!
     This time you are playing The God of War.
     It is too hard to control the character.
     You are very distressed.
@@ -115,8 +153,10 @@ scenes = {
     ]
 },
     
+    #Interactive fiction
     'No.9':{
-    'description':'''Hey, isn't Interactive Fiction similar to RPGs?
+    'description':'''Workshop9 -- Interactive Fiction 
+    Hey, isn't Interactive Fiction similar to RPGs?
     Can you solve the puzzle in Winchester's Nightmare?
     (Listen to the shell first!)
     Oh god, you spoil my game!
@@ -128,15 +168,16 @@ scenes = {
     ]
 },
     
+    #Digital poetry
     'No.4':{
-    'description':'''Poetry.
+    'description':'''Workshop4 -- Digital Poetry
+    Poetry.
     You said to yourself.
     You are never going to like it.
     Although thoses flying words look funny.
     Although you,
     Cannot understand them.
-    ''',
-    
+    ''',   
     'paths':[
     {'do':'No.2','phrase':'Look at No.2'},
     {'do':'No.10','phrase':'Look at No.10'},
@@ -144,8 +185,10 @@ scenes = {
     ]
 },
     
+    #MUDs
     'No.10':{
-    'description':'''Role-playing! I like it!
+    'description':'''Workshop10 -- MUDs
+    Role-playing! I like it!
     Emmmm.How about a wood elf with a pet wolf?
     Wait, you there, you can't eat my wolf!
     And whoever pretending to be the god, I suppose you need a rest?
@@ -158,8 +201,10 @@ scenes = {
     ]
 },
     
+    #first workshop
     'No.1':{
-    'description':'''Well, the beginning of workshops.
+    'description':'''Workshop1 -- Tactile Narratives
+    Well, the beginning of workshops.
     You are very nervous, because you know you are going to introduce yourself to other students.
     I am just an exchange student. What am I supposed to say?
     Surprisingly, Dave remembers your name after this very first class!
@@ -169,8 +214,10 @@ scenes = {
     ]
 },
     
+    #last workshop
     'No.11':{
-    'description':'''Welcome to the Final Web Project Assembly.
+    'description':'''Workshop11 -- Final Assembly
+    Welcome to the Final Web Project Assembly.
     You are taught to create links to other students'work.
     Is that the end? You ask yourself.
     'Yes, it is.'
